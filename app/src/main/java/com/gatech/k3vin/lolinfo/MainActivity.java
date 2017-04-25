ab1b8454163078e1f02d21f82be3f746f9159156
bang3 <bang3@gatech.edu>
2017-04-25 06:35:39 -0400

    Created going to E-Sports

--- app/src/main/java/com/gatech/k3vin/lolinfo/MainActivity.java
+++ app/src/main/java/com/gatech/k3vin/lolinfo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gatech.k3vin.lolinfo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -21,5 +22,11 @@ public class MainActivity extends ActionBarActivity{
 
     public void goToStatistics(View view) {
         startActivity(new Intent(MainActivity.this, Statistics.class));
+    }
+
+    public void goToESports(View view) {
+        Uri uriUrl = Uri.parse("http://www.lolesports.com/en_US/");
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
